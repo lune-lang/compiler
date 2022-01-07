03b0a8d1ed8406930f7b035274d62cd113c4aa8e
UnaryPlus <ombspring@gmail.com>
2022-01-07 09:56:51 -0500

    fix confusion synonym documentation

--- app/Document/Generate.hs
+++ app/Document/Generate.hs
@@ -11,8 +11,8 @@ import Data.Map (Map)
 import Syntax.Common
 import Syntax.Frontend
 
-spacesAfter :: [String] -> String
-spacesAfter = concatMap (++ " ")
+spacesBefore :: [String] -> String
+spacesBefore = concatMap (' ' :)
 
 h1 :: String -> String
 h1 text = concat [ "# ", text, "\n" ]
@@ -89,7 +89,7 @@ docSynonym :: String -> ModName -> Name -> [Name] -> String -> String
 docSynonym word modName name args body = let
   anchor = anchorLink modName name
   heading = h4 $ concat [ word, " ", docName name ]
-  code = codeBlock $ concat [ spacesAfter args, " = ", body ]
+  code = codeBlock $ concat [ docName name, spacesBefore args, " = ", body ]
   in concat [ anchor, heading, code ]
 
 anchorLink :: ModName -> Name -> String
